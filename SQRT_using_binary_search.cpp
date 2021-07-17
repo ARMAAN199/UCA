7a1ef81a49b8587da13597375c079bd9fc46059c
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-07-18 02:17:58 +0530

    binary search implemented

--- SQRT_using_binary_search.cpp
+++ SQRT_using_binary_search.cpp
@@ -7,32 +7,38 @@
 using namespace std;
 
 
-int binarysearch(int number, int L, int R) 
+float binarysearch(float number, float L, float R) 
 {
     if ( L > R )
     {
+        cout << "cancelling <<<<< MID IS "<< endl;
         return -1;
     }
-    int mid = ( L + R ) / 2;
+    float mid = ( L + R ) / 2;
     if (( mid * mid ) == number)
     {
+        cout << "FOUND <<<<< MID IS " << mid << endl;
         return mid;
     }
     if (( mid * mid ) < number)
     {
-        return binary_search(number, mid, R);
+        cout << "less <<<<< MID IS " << mid << endl;
+        return binarysearch(number, mid, R);
     }    
     else
     {
-        return binary_search(number, L, mid);
+        cout << "more <<<<< MID IS " << mid << endl;
+        return binarysearch(number, L, mid);
     }
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	int number, prec;
-    cin >> number, prec;
-    
+	float number;
+    int prec;
+    cin >> number;
+    cin >> prec;
+    cout << binarysearch(number, 0, number) << "\n";
 	return 0;
 }
