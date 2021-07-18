71061b7239839573cbaa8e3f25fb69026b7467d0
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-07-19 01:59:53 +0530

    Integer Part

--- SQRT_using_binary_search.cpp
+++ SQRT_using_binary_search.cpp
@@ -6,45 +6,16 @@
 // #include <bits/stdc++.h>
 using namespace std;
 
-
-float binarysearch(float number, int L, int R, float level) 
+double binarysearchdecimal(double number, double L, double R, double level) 
 {
-    float mid = ( L + R ) / 2;
-    if ( L > R )
-    {
-        cout << "OUT " << mid << " " << endl;
-        return -1;
-    }
-    if ((( mid * mid ) <= number) && (( (mid+level) * (mid+level) ) > number))
-    {
-        cout << "FOUND " << mid << " " << endl;
-        return mid;
-    }
-    if (( mid * mid ) < number)
-    {
-        cout << "LESS " << mid << " " << endl;
-        return binarysearch(number, mid, R, level);
-    }
-    else
-    {
-        cout << "MORE " << mid << " " << endl;
-        return binarysearch(number, L, mid, level);
-    }
-}
-
-float binarysearchdecimal(float number, float L, float R, float level) 
-{
-    // float mid = ( L + R ) / 2;
+    // double mid = ( L + R ) / 2;
     // cout << "MID" << (floor(((( L + R ) / 2) / level)))*level << endl;
-    float midterm = ( L + R ) / 2;
-    printf("%f\n", midterm);
-    float leveled = (midterm / level)+level;
-    printf("%f\n", leveled);
-    int floorval = int(leveled);
-    printf("%d\n", floorval);
-    float mid = (floorval * level);
+    double midterm = ( L + R ) / 2;
+    midterm =  level!=1? (midterm / level)+level : midterm;
+    midterm = int(midterm);
+    double mid = (midterm * level);
     printf("%f\n", mid);
-    // float mid = (int(((( L + R ) / 2.0) / level)))*level ;
+    // double mid = (int(((( L + R ) / 2.0) / level)))*level ;
     if ( L > R )
     {
         cout << "OUT " << mid << " " << endl;
@@ -70,12 +41,12 @@ float binarysearchdecimal(float number, float L, float R, float level)
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	float number;
+	double number;
     int prec;
     cin >> number;
     cin >> prec;
     // cout<<binarysearch(number, 0, number, 1) << "\n";
-    float curr_sol = binarysearch(number, 0, number, 1);
+    double curr_sol = binarysearchdecimal(number, 0, number, 1);
     double level = 0.1;
     for(int i=0; i<prec; i++)
     {
