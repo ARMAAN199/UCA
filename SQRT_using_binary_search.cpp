bed2475189d0d893fdcfe52cc3e4f93c49783437
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-07-19 02:09:25 +0530

    Cleaning Code

--- SQRT_using_binary_search.cpp
+++ SQRT_using_binary_search.cpp
@@ -3,13 +3,9 @@ using namespace std;
 
 double binarysearchdecimal(double number, double L, double R, double level) 
 {
-    // double mid = ( L + R ) / 2;
-    // cout << "MID" << (floor(((( L + R ) / 2) / level)))*level << endl;
     double mid = ( L + R ) / 2;
     mid =  level!=1? (mid / level)+level : mid;
     mid = (int(mid)*level);
-    printf("%f\n", mid);
-    // double mid = (int(((( L + R ) / 2.0) / level)))*level ;
     if ( L > R )
     {
         return -1;
@@ -31,18 +27,15 @@ double binarysearchdecimal(double number, double L, double R, double level)
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	double number;
+	double number, level =0.1;
     int prec;
-    cin >> number;
-    cin >> prec;
-    // cout<<binarysearch(number, 0, number, 1) << "\n";
+    cin >> number >> prec;
     double curr_sol = binarysearchdecimal(number, 0, number, 1);
-    double level = 0.1;
     for(int i=0; i<prec; i++)
     {
         curr_sol = binarysearchdecimal(number, curr_sol, (curr_sol+(level*10)), level);
         level /= 10;
-        cout << "OUTPUT =" <<curr_sol << endl;
     }
+    cout << curr_sol << endl;
 	return 0;
 }
