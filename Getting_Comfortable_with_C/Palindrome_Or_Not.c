c27c364668e141fd03aea8f337abf49e95ecaf79
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-08-06 00:49:23 +0530

    Assignment

--- Getting_Comfortable_with_C/Palindrome_Or_Not.c
+++ Getting_Comfortable_with_C/Palindrome_Or_Not.c
@@ -1,28 +1,60 @@
+/* C program to Check whether String is palindrome or not ignoring cases and spaces
+ *
+ *Author - Armaan Jain 1910990432
+ *Input - A String
+ *Sample Input - Never Odd or Even
+ *Sample Output - true
+ *Assignment: Assignment 1 - C Foundations
+ */
+
 #include <stdio.h>
+
 int main()
 {
-  char text[100];
-  int begin, middle, end, length = 0;
+  /* Scanning a String until newline is read */
+  char input[1000],str[1000];
+  scanf("%[^\n]s",input);
 
-  gets(text);
-
-  while (text[length] != '\0')
-    length++;
-
-  end = length - 1;
-  middle = length/2;
-
-  for (begin = 0; begin < middle; begin++)
+  int isPalindrome = 1, length = 0,i=0;
+  
+  while (input[i] != '\0')
   {
-    if (text[begin] != text[end])
+    //Removing all spaces and changing all characters to lowercase
+    if(input[i] == ' ')
     {
-      printf("Not a palindrome.\n");
+      i++;
+      continue;
+    }
+    if(input[i]<97)
+    {
+      input[i] += 32;
+    }
+    str[length] = input[i];
+    i++;
+    
+    //Counting the length of the final string
+    length++;
+  }
+
+  int right = length - 1, mid = length/2;
+  for (int left = 0; left < mid; left++)
+  {
+    if (str[left] != str[right])
+    {
+      isPalindrome = 0;
       break;
     }
-    end--;
+    right--;
   }
-  if (begin == middle)
-    printf("Palindrome.\n");
+
+  if(isPalindrome == 0)
+  {
+    printf("false");
+  }
+  else
+  {
+    printf("true");
+  }
 
   return 0;
 }
