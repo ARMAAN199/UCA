7c40609c7a65238cbbf99d62bd08b4e033c9f628
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-08-13 23:55:30 +0530

    Assignment 7

---
+++ sorting_ass_7/inversion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i = 0; i<size; i++)
+    {
+       scanf("%d",&arr[i]); 
+    }
+    int count=0;
+    int j=0;
+    for(int i=1;i<size;i++)
+    {
+        int curr=arr[i];
+        for(j=i-1;j>=0;j--)
+        {
+            if(arr[j]>curr)
+            {
+                arr[j+1]=arr[j];
+                count++;
+            }
+        }
+        arr[j+1]=curr;
+    }
+   printf("%d",count);
+   return 0;
+}
