79157e5d30d8b424e1a16c9ccb14c45c42cdcc07
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-08-18 20:52:08 +0530

    Assignment3

---
+++ assignment_3_bits_and_bytes/1.c
@@ -0,0 +1,24 @@
+/* BitAnd
+ *
+ *Author - Armaan Jain 1910990432
+ *Input - 2 integers
+ *Assignment: Assignment2_Bits_Bytes
+ */
+
+/* Legal ops:  ~ |  
+/* Max ops: 8
+*/
+
+int bitAnd (int x, int y) {
+     return ~(~x|~y);
+}
+
+int main(){
+    int a,b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    //Function call that runs custom bitand.
+    printf("%d",bitand(a,b));
+    
+}
