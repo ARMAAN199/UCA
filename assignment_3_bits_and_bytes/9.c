1f877464a4fdc84fb518f5a40d5a8d8637b2f39a
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-08-18 21:23:00 +0530

    Assignment3

--- assignment_3_bits_and_bytes/9.c
+++ assignment_3_bits_and_bytes/9.c
@@ -1,9 +1,7 @@
 /* hackerrank Question
  *
  *Author - Armaan Jain 1910990432
- *Input - 2 integers
  *Assignment: Assignment2_Bits_Bytes
  */
-
-
-
+ 
+ //At this point I've only got it partially done. I'll upload as soon as I'm done. In less than 12 hours probably. 
